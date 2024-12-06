e61998e26ce64bcc63f72f925105c8c904f62606
Clément <clementpradon@develop.ovh>
2024-12-06 07:01:31 +0100

    valid

--- process/connectBdd.php
+++ process/connectBdd.php
@@ -7,7 +7,7 @@ class PDOMySQLConnector
     public static function getClient()
     {
         if (self::$mysqlClient == null) {
-            self::$mysqlClient = new PDO('mysql:host=localhost;dbname=lanathomiedeposeidon_prodndi;charset=utf8', '389417', 'jlpab5812');
+            self::$mysqlClient = new PDO('mysql:host=mysql-lanathomiedeposeidon.alwaysdata.net;dbname=lanathomiedeposeidon_prodndi;charset=utf8', '389417', 'jlpab5812');
         }
         return self::$mysqlClient;
     }
