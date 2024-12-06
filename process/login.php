13dc847f36d72ee076f0654ab8b8bfaeb45d3b2c
clementpradon <clementpradon@develop.ovh>
2024-12-06 02:32:28 +0000

    update

--- process/login.php
+++ process/login.php
@@ -7,5 +7,11 @@ $password =$_POST['password'];
 
 $result = user::loginUser($username, $password);
 
+if ($password === $result[0]['password']) {
+    $_ENV['username'] = $username;
+    $_ENV['idUser'] = $result[0]['idUser'];
+    $_ENV['connected'] = true;
+}
+
 header('Content-Type: application/json');
 echo json_encode($result);
